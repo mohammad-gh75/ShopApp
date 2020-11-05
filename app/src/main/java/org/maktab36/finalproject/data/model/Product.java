51e32e2ce49bdc78f00731c5e947a2cdc305fb03
mohammad-gh75 <ar.arsham@yahoo.com>
2020-11-05 17:02:17 +0330

    create Product model and add required fields. complete ProductDeserializer to deserialize json to Product Model.

--- app/src/main/java/org/maktab36/finalproject/data/model/Product.java
+++ app/src/main/java/org/maktab36/finalproject/data/model/Product.java
@@ -1,4 +1,73 @@
 package org.maktab36.finalproject.data.model;
 
+import java.util.List;
+
 public class Product {
+    private int mId;
+    private String mName;
+    private String mDescription;
+    private String mPrice;
+    private String mLinkPath;
+    private List<String> mImagesUrl;
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public void setPrice(String price) {
+        mPrice = price;
+    }
+
+    public String getLinkPath() {
+        return mLinkPath;
+    }
+
+    public void setLinkPath(String linkPath) {
+        mLinkPath = linkPath;
+    }
+
+    public List<String> getImagesUrl() {
+        return mImagesUrl;
+    }
+
+    public void setImagesUrl(List<String> imagesUrl) {
+        this.mImagesUrl = imagesUrl;
+    }
+
+    public Product() {
+    }
+
+    public Product(int id, String name, String description,
+                   String price, String linkPath, List<String> imagesUrl) {
+        mId = id;
+        mName = name;
+        mDescription = description;
+        mPrice = price;
+        mLinkPath = linkPath;
+        mImagesUrl = imagesUrl;
+    }
 }
--- app/src/main/java/org/maktab36/finalproject/data/remote/retrofit/ProductDeserializer.java
+++ app/src/main/java/org/maktab36/finalproject/data/remote/retrofit/ProductDeserializer.java
@@ -1,13 +1,16 @@
 package org.maktab36.finalproject.data.remote.retrofit;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import org.maktab36.finalproject.data.model.Product;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDeserializer implements JsonDeserializer<List<Product>> {
@@ -17,8 +20,39 @@ public class ProductDeserializer implements JsonDeserializer<List<Product>> {
             Type typeOfT,
             JsonDeserializationContext context) throws JsonParseException {
 
+        List<Product> products = new ArrayList<>();
+        JsonArray productArray = json.getAsJsonArray();
+        for (int i = 0; i <productArray.size() ; i++) {
+            JsonObject productObject = productArray.get(i).getAsJsonObject();
 
+            int id=productObject.get("id").getAsInt();
+            String name=productObject.get("name").getAsString();
+            String description=productObject.get("description").getAsString();
+            String price=productObject.get("price").getAsString();
+            String linkPath=getProductLink(productObject);
+            List<String> imagesUrl=getProductImagesUrl(productObject);
 
-        return null;
+            Product product=new Product(id,name,description,price,linkPath,imagesUrl);
+            products.add(product);
+        }
+        return products;
+    }
+
+    private String getProductLink(JsonObject productObject){
+        JsonObject link=productObject.get("_links").getAsJsonObject();
+        JsonArray self=link.get("self").getAsJsonArray();
+        JsonObject href=self.get(0).getAsJsonObject();
+        return href.get("href").getAsString();
+    }
+
+    private List<String> getProductImagesUrl(JsonObject productObject){
+        List<String> imagesUrl=new ArrayList<>();
+        JsonArray images = productObject.get("images").getAsJsonArray();
+        for (int i = 0; i <images.size() ; i++) {
+            JsonObject image=images.get(i).getAsJsonObject();
+            String link=image.get("src").getAsString();
+            imagesUrl.add(link);
+        }
+        return imagesUrl;
     }
 }
